9c6105331481391787bfbc217c965ff1ba072b0e
Daniel Fang <danfang@uw.edu>
2015-05-29 02:05:00 -0700

    Refactoring files

--- dist/s3-sync.sh
+++ dist/s3-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync . s3://danielfang.org/ --exclude '*' --include '*.css' --include 'img/*' --include '*.html' --exclude 'node_modules/*' --acl public-read-write
+aws s3 sync . s3://danielfang.org/ --acl public-read-write
